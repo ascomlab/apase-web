5bfba1662a3fb828785945c50e9c59732877c2e3
ascomlab <43664288+ascomlab@users.noreply.github.com>
2026-04-09 11:53:26 +0700

    Update data.ts

--- app/data.ts
+++ app/data.ts
@@ -1,21 +1,31 @@
 export const importantDate = [
   {
+    date: "MAY 01 2026",
+    title: "Paper Submission Opens",
+    description: "Submission portal opens for authors",
+  },
+  {
     date: "JUL 31 2026",
-    title: "Full Paper Deadline",
-    description: "Replace with the actual deadline for paper submission.",
+    title: "Full Paper Submission Deadline",
+    description: "Deadline for full paper submission",
   },
   {
     date: "SEP 15 2026",
-    title: "Review result",
-    description: "Acceptance results sent to authors",
+    title: "Notification of Acceptance",
+    description: "Deadline to notify authors of acceptance results",
   },
   {
     date: "OCT 15 2026",
     title: "Camera-Ready Paper Submission",
-    description: "Revised final papers due",
+    description: "Deadline for submitting the camera-ready version",
   },
   {
-    date: "DEC 20–22 2026",
+    date: "SEP 15 - OCT 31 2026",
+    title: "Registration",
+    description: "Period for registration",
+  },
+  {
+    date: "DEC 18–21 2026",
     title: "Conference Dates",
     description: "APASE 2026 conference in Harbin, China",
   },
