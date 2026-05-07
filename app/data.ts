dc29f7a867998c2e657366804684a0b3603b2aae
ascomlab <43664288+ascomlab@users.noreply.github.com>
2026-05-07 14:49:24 +0700

    Update data.ts

--- app/data.ts
+++ app/data.ts
@@ -1,22 +1,22 @@
 export const importantDate = [
  
   {
-    date: "JUL 31 2026",
+    date: "JUL 31 2026(TBC)",
     title: "Full Paper Submission Deadline",
     description: "Deadline for full paper submission",
   },
   {
-    date: "SEP 15 2026",
+    date: "SEP 15 2026(TBC)",
     title: "Notification of Acceptance",
     description: "Deadline to notify authors of acceptance results",
   },
   {
-    date: "OCT 15 2026",
+    date: "OCT 15 2026(TBC)",
     title: "Camera-Ready Paper Submission",
     description: "Deadline for submitting the camera-ready version",
   },
   {
-    date: "SEP 15 - OCT 31 2026",
+    date: "SEP 15 - OCT 31 2026(TBC)",
     title: "Registration",
     description: "Period for registration",
   },
