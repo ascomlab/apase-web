b1059fb8c4d122cdbbe3fd9295d8e0a399352d78
Llam4u <h.warunyu@gmail.com>
2026-07-15 08:51:51 +0700

    remove unused speaker

--- app/keynote-speakers/speakers.ts
+++ app/keynote-speakers/speakers.ts
@@ -27,20 +27,4 @@ export const speakers: Speaker[] = [
     abstract:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
   },
-  {
-    id: "speaker-2",
-    name: "Prof. XXXX XXXX",
-    title: "Lead Technology Officer",
-    subtitle: "(TBD) Advanced Systems Corporation",
-    image: "/images/keynote-speakers/profile-placeholder.jpg",
-    imageAlt: "Professional portrait of a speaker",
-    quote:
-      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.",
-    bio: [
-      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
-      "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
-    ],
-    abstract:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.",
-  },
 ];
