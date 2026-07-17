35a58404d68c97faa2f6796cf811f21d74886eec
ascomlab <43664288+ascomlab@users.noreply.github.com>
2026-07-17 14:37:01 +0700

    Update data.ts

--- app/submission/data.ts
+++ app/submission/data.ts
@@ -1,10 +1,10 @@
 export const submissionGuidelinesContent = `# Paper Submission Guidelines
 
-The Asia-Pacific Aerospace Science & Engineering Symposium(APASE) 2026 will be published by IOP in the open access [**Journal of Physics: Conference Series (JPCS).**](https://iopscience.iop.org/journal/1742-6596) The registration fee includes the publication of one paper. All papers submitted will undergo a peer-review process. Submission accepted for contributed orals and posters have an 8-page limit. References are not included in the page limit.
+The Asia-Pacific Aerospace Science & Engineering Symposium(APASE) will be published in the open access. The registration fee includes the publication of one paper. All papers submitted will undergo a peer-review process. Submission accepted for contributed orals and posters have an 8-page limit. References are not included in the page limit.
 
-## JPCS Guidelines:
+## Guidelines:
 
-Full details of the author publication guidelines for JPCS can be found [**here**](https://publishingsupport.iopscience.iop.org/author-guidelines-for-conference-proceedings/).  The following lists the essential requirements for an article to be published in an IOP: Conference Series journal: 
+Full details of the author publication guidelines can be found [**here**](https://publishingsupport.iopscience.iop.org/author-guidelines-for-conference-proceedings/).  The following lists the essential requirements for an article to be published : Conference Series journal: 
 
 - the page size should be A4
 - each page should have clear margins of 4cm (top), 2.5cm (left and right) and 2.7cm (bottom) 
@@ -12,13 +12,13 @@ Full details of the author publication guidelines for JPCS can be found [**here*
 - all articles must contain an abstract 
 - in the PDF, all fonts should be embedded 
 
-The IOP Conference Series template is mandatory for submission. It can be found below:
+The Conference Series template is mandatory for submission. It can be found below:
 
 - [**Microsoft Word**](https://cms.iopscience.iop.org/alfresco/d/d/workspace/SpacesStore/f67538ae-18b2-11e4-831a-29411a5deefe/WordGuidelines.zip)
 - [**LaTeX**](https://cms.iopscience.iop.org/alfresco/d/d/workspace/SpacesStore/a83f1ab6-cd8f-11e0-be51-5d01ae4695ed/LaTeXTemplates.zip)
 - [**Overleaf**](https://www.overleaf.com/read/qhbdnjyswhxg#7770de)
 
-Ensure that within the template, all other JPCS formatting rules are still followed. The paper should be submitted as an editable and not password protected PDF. 
+Ensure that within the template, all other formatting rules are still followed. The paper should be submitted as an editable and not password protected PDF. 
 
 **It is important to ensure that when you submit your paper, it is in its final form ready for publication, and has been thoroughly proofread. IOP do not copy edit or reformat papers and will not send out author proofs prior to publication.** Post-publication changes are not permitted, so please ensure that your paper has been checked for errors.
 `;
