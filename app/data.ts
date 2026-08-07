61517d8614e30236f8a5cc8d4eb8b208bd107024
ascomlab <43664288+ascomlab@users.noreply.github.com>
2026-08-07 10:15:53 +0700

    Update data.ts

--- app/data.ts
+++ app/data.ts
@@ -1,17 +1,17 @@
 export const importantDate = [
  
   {
-    date: "SEP 30 2026",
+    date: "Nov 8 2026",
     title: "Paper Submission Deadline",
     description: "Deadline for full paper submission",
   },
   {
-    date: "OCT 30 2026",
+    date: "Nov 26 2026",
     title: "Notification of Acceptance",
     description: "Deadline to notify authors of acceptance results",
   },
     {
-    date: "Nov 15 2026",
+    date: "Dec 8 2026",
     title: "Early Registration Deadline",
     description: "Deadline for registration",
   },
